ab1e5ad1521f2ad604306dbff3f627c187dae0b3
Nathan Stitt <nathan@stitt.org>
2014-02-20 16:28:48 -0600

    Postal Addresses migration, tests, and model

---
+++ lib/skr/address.rb
@@ -0,0 +1,94 @@
+module Skr
+
+    # A postal address with optional email and phone number
+    # By default all fields may be left blank.
+    #
+    # Validations may be selectively enabled by using the #enable_validations method
+    class Address < Skr::Model
+
+        # @!attribute ensure_not_blank
+        #   Must the Address be filled out?  The {#blank?} method must return false
+        #   @return [Boolean]
+        # @!attribute validate_email
+        #   Will the email be validated to meet the {EmailValidator} requirements?
+        #   @return [Boolean]
+        # @!attribute validate_phone
+        #   Should the phone field be validated to be not blank?
+        #   @return [Boolean]
+        attr_accessor :ensure_not_blank, :validate_email, :validate_phone
+
+        validates  :name, :line1, :city, :state, :postal_code, :presence=>true, :if=>:ensure_not_blank
+        validates  :email, :presence=>true, :email=>true, :if=>:validate_email
+        validates  :phone, :presence=>true, :if=>:validate_phone
+
+        # @return [Address] a blank copy of an address
+        def self.blank
+            Address.new({ name: '', line1: '', city: '', state: '', postal_code: '' })
+        end
+
+        # @return [String] the name and email formatted for inclusion in an email
+        def email_with_name
+            "#{name} <#{email}>"
+        end
+
+        # fill in missing fields from postal_code using the ZipCode lookup table
+        def fill_missing_from_zip
+            if ( self.postal_code.present? &&
+                 ( self.city.blank? || self.state.blank? ) &&
+                 zc = ZipCode.find_by_code( self.postal_code )
+               )
+                self.city  ||= zc.city
+                self.state ||= zc.state
+            end
+        end
+
+        # @return [Boolean] is any of (name line1 city state postal_code) blank?
+        def blank?
+            return !! %w{ name line1 city state postal_code }.detect{ |field| self[field].blank? }
+        end
+
+        # split the name on space
+        # @return [Hash]
+        #   * :first [String] Portion of name before first space
+        #   * :last  [String] Portion of name after last space
+        def seperated_name
+            {:first=>name.to_s.split(' ').first, :last=> name.to_s.split(' ').last }
+        end
+
+        # enable selected validations
+        # @option options [Boolean] :include_email should the email be validated
+        # @option options [Boolean] :include_phone should the phone number be validated
+        def enable_validations( options = {} )
+            self.ensure_not_blank = true
+            self.validate_email = options[:include_email]
+            self.validate_phone = options[:include_phone]
+        end
+
+        # @param extra_fields [Array] list of extra fields to include in the address
+        # @return [String] Address converted to string, formatted with line breaks in the typical US style of display
+        # @example
+        #     address = Address.new( name: 'Bob\s Uncle',phone: '877-555-5555', line1: 'PO Box 87',
+        #                            city: 'Nowhereville', state: 'Urgandishly' postal_code: 'ASCN 1ZZ' )
+        #     address.to_s( :include => :phone ) #=>
+        #             Bob's Uncle
+        #             PO Box 87
+        #             Nowhereville, Urgandishly ASCN 1ZZ
+        #             877-5550-5555
+        def to_s( include: [] )
+            ret = ""
+            %w{ name line1 line2 }.each{ |a| ret << self[a] + "\n" unless self[a].blank? }
+            ret << city.to_s
+            ret << ' ' + state unless state.blank?
+            ret << ', ' + postal_code.to_s unless postal_code.blank?
+            include = [ *include ]
+            if include.any?
+                ret << "\n" + include.map{ | field | self[ field ] }.join(' ')
+            end
+            ret
+        end
+
+
+    end
+
+
+end # end Skr module
---
+++ test/address_test.rb
@@ -0,0 +1,65 @@
+require_relative 'test_helper'
+
+class AddressTest < Skr::TestCase
+
+
+    def test_creation
+        addr=Address.new( name:" a test", email:"test@test.com" )
+        addr.save.must_equal true
+    end
+
+    def test_email_with_name
+        addr=Address.new( email: 'john@mcauthor.com', name: 'John D. McAuthor' )
+        assert_equal "John D. McAuthor <john@mcauthor.com>", addr.email_with_name
+    end
+
+    def test_filling_from_zip
+        addr = Address.new( postal_code: '65109' )
+        addr.fill_missing_from_zip
+        assert_equal 'Jefferson City', addr.city
+        assert_equal 'MO', addr.state
+    end
+
+    def test_blankness
+        addr = Address.blank
+        assert addr.blank?, "Blank address isn't blank?"
+        %w{ name line1 city state postal_code }.each do | attr |
+            assert addr.blank?
+            addr[ attr ] = "something"
+        end
+        refute addr.blank?
+    end
+
+    def test_separated_name
+        addr=Address.new( name: 'John D McAuthor' )
+        assert_equal 'John', addr.seperated_name[:first]
+        assert_equal 'McAuthor', addr.seperated_name[:last]
+    end
+
+    def test_selective_validations
+        addr=Address.blank
+        assert addr.save
+        addr.enable_validations
+        refute addr.save
+        %w{ name line1 city state postal_code }.each do | attr |
+            addr[ attr ] = "something"
+        end
+        assert addr.save
+        addr.enable_validations include_email: true
+        refute addr.save
+        addr.email = "test" # <- no '@'
+        refute addr.save
+        addr.email = "test@test.com" # <- no '@'
+        assert addr.save
+        addr.enable_validations include_phone: true
+        refute addr.save
+        addr.phone = "555-555-5555"
+        assert addr.save
+    end
+
+    def test_converting_to_string
+        addr = skr_addresses(:home)
+        assert_equal "home\n12727 Southview Dr\nHolts Summit Missouri, 65043", addr.to_s
+    end
+
+end
