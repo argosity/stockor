f11e0854074a7fdaee8f50b12bf616f4aa09d860
Nathan Stitt <nathan@stitt.org>
2014-03-30 18:42:53 -0500

    Add credit limit

--- db/migrate/20140220203029_create_skr_customers.rb
+++ db/migrate/20140220203029_create_skr_customers.rb
@@ -4,11 +4,13 @@ class CreateSkrCustomers < ActiveRecord::Migration
     def change
 
         create_skr_table "customers" do |t|
+            t.skr_code_identifier
             t.skr_reference :billing_address,        null: false, to_table: :addresses
             t.skr_reference :shipping_address,       null: false, to_table: :addresses
             t.skr_reference :terms,                  null: false, to_table: :payment_terms
             t.skr_reference :gl_receivables_account, null: false, to_table: :gl_accounts
-            t.string   "code",                       null: false
+            t.skr_currency :credit_limit,            default: 0.0
+            t.skr_currency :open_balance,            default: 0.0
             t.string   "hash_code",                  null: false
             t.string   "name",                       null: false
             t.text     "notes"
