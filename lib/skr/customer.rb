da96b75d940674f9bb17b68c74c902cab3ca68d3
Nathan Stitt <nathan@stitt.org>
2014-03-30 19:16:45 -0500

    Customer set GlAccount from the configuration

--- lib/skr/customer.rb
+++ lib/skr/customer.rb
@@ -8,16 +8,16 @@ module Skr
         # Common code shared with {Vendor}
         include BusinessEntity
 
-        belongs_to :gl_receivables_account, :class_name=>'GlAccount'
-        export_associations :gl_receivables_account
+        belongs_to :gl_receivables_account, :class_name=>'GlAccount', export: true
 
         delegate_and_export  :gl_receivables_account_number
+
         validates :gl_receivables_account, :set=>true
 
         private
 
         def set_defaults
-            self.gl_receivables_account ||= GlAccount.find_by_number( Skr::Core.config.default_gl_ar_account_number )
+            self.gl_receivables_account ||= GlAccount.default_for( :ar )
         end
     end
 
