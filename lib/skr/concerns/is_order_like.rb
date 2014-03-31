1c6f741d8ab8a175e9b6978e2d646f2181362764
Nathan Stitt <nathan@stitt.org>
2014-03-30 19:02:10 -0500

    Concerns for orders.  They hold common code for Quotes, SalesOrders, Invoices, and PickTickets

---
+++ lib/skr/concerns/is_order_like.rb
@@ -0,0 +1,51 @@
+module Skr
+    module Concerns
+
+        module IsOrderLike
+            extend ActiveSupport::Concern
+
+            module InstanceMethods
+
+                def other_charge_lines
+                    self.lines.select{|l| l.sku.is_other_charge? }
+                end
+
+                def regular_lines
+                    self.lines.reject{|l| l.sku.is_other_charge? }
+                end
+
+                def regular_lines_total
+                    self.regular_lines.sum{|l|l.total}
+                end
+
+                def subtotal
+                    self.regular_lines.inject(0){|sum,line| sum + line.total }
+                end
+
+                def total
+                    if total = self.read_attribute('total')
+                        BigDecimal.new(total)
+                    elsif self.new_record? || self.association(:lines).loaded?
+                        self.lines.inject( BigDecimal.new('0') ){|sum,line| sum += line.total }
+                    else
+                        BigDecimal.new( self.lines.sum('price*qty') )
+                    end
+                end
+
+
+            end
+
+            module ClassMethods
+
+                def is_order_like
+                    self.send :include, InstanceMethods
+
+                    validates_associated :lines
+                    export_methods :total
+                end
+
+            end
+
+        end
+    end
+end
---
+++ lib/skr/concerns/is_order_line.rb
@@ -0,0 +1,60 @@
+module Skr
+    module Concerns
+
+        module IsOrderLine
+            extend ActiveSupport::Concern
+
+            module InstanceMethods
+                def uom_record
+                    if self.association(:sku).loaded? && self.sku.association(:uoms).loaded?
+                        self.sku.uoms.detect{|uom| uom.code == self.uom_code }
+                    else
+                        self.sku.uoms.where({ code: self.uom_code }).first
+                    end
+                end
+
+                def is_other_charge?
+                    self.sku.is_other_charge?
+                end
+
+                def total
+                    self.price * self.qty
+                end
+
+                private
+
+                def ensure_sku_does_not_change
+                    errors.add(:sku, "can not be updated") if changes['sku_code']
+                    if change = changes['sku_loc_id']
+                        # allow if the sku_id is the same on both old & new locations
+                        unless 1 == SkuLoc.where( id: change ).pluck('sku_id').uniq.length
+                            errors.add(:sku, "must be the same in both locations") if sku_code_changed?
+                        end
+                    end
+                end
+            end
+
+            module ClassMethods
+
+                def is_order_line( options = {} )
+                    self.send :include, InstanceMethods
+
+                    validate  :ensure_sku_does_not_change, :on=>:update
+
+                    if options[:parent]
+                        before_create do
+                            self.position ||= ( self.send(options[:parent]).lines.maximum(:position) || 0 ) + 1
+                        end
+                    end
+
+                    export_methods :total, :optional=>false
+
+                end
+
+
+            end
+
+        end
+
+    end
+end
