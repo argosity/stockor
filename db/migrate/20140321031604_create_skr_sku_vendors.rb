2e1e8d93d7e3326f30c0247ba688f6e4eede6f1b
Nathan Stitt <nathan@stitt.org>
2014-03-30 21:03:33 -0500

    reindent code

--- db/migrate/20140321031604_create_skr_sku_vendors.rb
+++ db/migrate/20140321031604_create_skr_sku_vendors.rb
@@ -4,14 +4,14 @@ class CreateSkrSkuVendors < ActiveRecord::Migration
     def change
 
         create_skr_table "sku_vendors" do |t|
-            t.skr_reference :sku,        null: false, single: true
-            t.skr_reference :vendor,     null: false, single: true
-            t.skr_currency  "list_price",null: false, precision: 15, scale: 2
-            t.string   "part_code",      null: false
-            t.boolean  "is_active",      null: false, default: true
-            t.integer  "uom_size",       null: false, default: 1
-            t.string   "uom_code",       null: false, default: 'EA'
-            t.decimal  "cost",           null: false, precision: 15, scale: 2
+            t.skr_reference :sku,              null: false, single: true
+            t.skr_reference :vendor,           null: false, single: true
+            t.skr_currency  "list_price",      null: false, precision: 15, scale: 2
+            t.string   "part_code",            null: false
+            t.boolean  "is_active",            null: false, default: true
+            t.integer  "uom_size",             null: false, default: 1
+            t.string   "uom_code",             null: false, default: 'EA'
+            t.decimal  "cost",                 null: false, precision: 15, scale: 2
             t.skr_track_modifications
         end
     end
