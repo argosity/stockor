ef22d34ac02bd402371452449d45b8490e268be6
Nathan Stitt <nathan@stitt.org>
2014-04-13 22:08:38 -0500

    Extensions for SalesOrder line association

---
+++ lib/skr/concerns/so_extensions.rb
@@ -0,0 +1,30 @@
+module Skr
+    module Concerns
+
+        module SO
+
+            module Lines
+
+                def set_ship_qty
+                    each{|l| l.qty_to_ship = l.qty }
+                end
+
+                def eq_qty
+                    if proxy_association.loaded?
+                        inject(0){ | sum, sol | sum + (sol.eq_qty*uom_size) }
+                    else
+                        sum('qty*uom_size')
+                    end
+                end
+
+                def eq_qty_allocated
+                    if proxy_association.loaded?
+                        inject(0){ | sum, sol | sum + (sol.qty_allocated * uom_size) }
+                    else
+                        sum('qty_allocated*uom_size')
+                    end
+                end
+            end
+        end
+    end
+end
