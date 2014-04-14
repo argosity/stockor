833ab7b5a428bd0630ab8c2f7a03ba4185e860af
Nathan Stitt <nathan@stitt.org>
2014-04-13 21:43:05 -0500

    Re-Arrange migrations so foreign keys align

--- db/migrate/20140400164729_create_skr_vouchers.rb
+++ db/migrate/20140400164729_create_skr_vouchers.rb
@@ -5,15 +5,17 @@ class CreateSkrVouchers < ActiveRecord::Migration
 
         create_skr_table "vouchers" do |t|
             t.skr_visible_id
+
+            t.skr_reference :vendor,         single: true, null: false
             t.skr_reference :purchase_order, single: true, null: true
-            t.skr_reference :vendor,         single: true
+
             t.skr_reference :terms,          to_table: 'payment_terms'
-            t.skr_reference :location,       single: true
+
             t.string   "state",              null: false
             t.string   "refno"
-            t.skr_currency :freight,         null: false, default: 0.0
+
             t.date     "confirmation_date",     null: true # starts out as non-confirmed
-            t.skr_track_modifications create_only: true
+            t.skr_track_modifications
         end
 
     end
--- db/migrate/20140400164733_create_skr_vo_lines.rb
+++ db/migrate/20140400164733_create_skr_vo_lines.rb
@@ -2,12 +2,10 @@ require 'skr/core/db/migration_helpers'
 
 class CreateSkrVoLines < ActiveRecord::Migration
     def change
-
         create_skr_table "vo_lines" do |t|
             t.skr_reference :voucher,        null: false, single: true
-            t.skr_reference :po_line,        null: true,  single: true
-            t.skr_reference :sku_loc,        null: false, single: true
             t.skr_reference :sku_vendor,     null: false, single: true
+            t.skr_reference :po_line,        null: true,  single: true
             t.string   "sku_code",           null: false
             t.string   "part_code",          null: false
             t.string   "description",        null: false
@@ -16,8 +14,7 @@ class CreateSkrVoLines < ActiveRecord::Migration
             t.integer  "position",           null: false, limit: 2
             t.integer  "qty",                null: false, default: 0
             t.skr_currency  "price",         null: false
-            t.skr_track_modifications create_only: true
-
+            t.skr_track_modifications
         end
 
     end
