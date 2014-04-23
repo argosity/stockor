ec0a02d8b56a90fa521408680aaa49f4af90d828
Nathan Stitt <nathan@stitt.org>
2014-04-22 19:18:27 -0500

    Default terms & location to customer when it's set

--- lib/skr/concerns/is_order_like.rb
+++ lib/skr/concerns/is_order_like.rb
@@ -6,11 +6,23 @@ module Skr
 
             module InstanceMethods
 
+
+                # Set's the customer.  It also defaults the terms, addresses,and tax_exempt status to the customer's defaults
+                # @param cust [Customer]
+                # @return Customer
+                def customer=(cust)
+                    super
+                    self.terms ||= cust.terms
+                    self.is_tax_exempt    = cust.is_tax_exempt        if     self.is_tax_exempt.nil?
+                    self.billing_address  = cust.billing_address.dup  unless self.billing_address.present?
+                    self.shipping_address = cust.shipping_address.dup unless self.shipping_address.present?
+                end
+
                 protected
 
-                def set_defaults
+                def set_order_defaults
                     self.location ||= Location.default
-                    self.terms    ||= customer.terms
+                    self.terms    ||= customer.terms if self.customer
                     true
                 end
 
@@ -21,9 +33,11 @@ module Skr
                 def is_order_like
                     self.send :include, InstanceMethods
                     has_sku_loc_lines # pull in the sku_loc_lines module
+
                     validates_associated :lines
-                    export_methods :total
-                    before_validation :set_defaults, :on=>:create
+
+                    before_validation :set_order_defaults, :on=>:create
+
                 end
 
             end
