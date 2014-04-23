8a192bb5c524376c6eb0cc63c16e70eb46da88b4
Nathan Stitt <nathan@stitt.org>
2014-04-22 20:37:17 -0500

    test to ensure picking doesn't exceed qty

--- test/so_line_test.rb
+++ test/so_line_test.rb
@@ -10,21 +10,13 @@ class SoLineTest < Skr::TestCase
             description:'A Test', price: 12.42
           })
         sol.uom = skr_uoms('HATEA')
-        sol.save.must_equal true
-    end
-
-    def test_picking_qty
-        sol = skr_so_lines(:first_string)
-        sol.sku_loc.allocate_available_qty!
-        pt = sol.sales_order.pick_tickets.build
-        assert_saves pt
-        assert_equal 2, sol.reload.qty_picking
+        assert_saves sol
     end
 
     def test_scopes
         sol = skr_so_lines(:first_string)
         sol.sku_loc.allocate_available_qty!
-        assert_equal 3, SoLine.pending.count
+        assert_equal 3, SoLine.open.count
         assert_equal 3, SoLine.allocated.count
         assert_equal 2, SoLine.unallocated.count
         assert_equal 1, SoLine.pickable.count
@@ -38,7 +30,29 @@ class SoLineTest < Skr::TestCase
         assert_difference 'sku_loc.reload.qty_allocated',2 do
             assert_saves so
         end
+    end
 
+    def test_overcreate_prevented
+        so = skr_sales_orders(:first)
+        skr_sku_locs(:stringdefault).allocate_available_qty!
+        pt = so.pick_tickets.build
+
+        refute_empty pt.lines
+        assert_saves pt
+
+        empty_pt = so.pick_tickets.build
+        assert_empty empty_pt.lines
+        refute_saves empty_pt
+
+        # This is a corner case
+        # if the pt is created without referencing the SO
+        # it can sneak through.  Not sure if we should prevent
+        # this or not, I mean.. maybe you want/need this?
+        manual_pt = PickTicket.new( sales_order: so )
+        manual_pt.lines.build({
+            so_line: pt.lines.first.so_line
+          })
+        assert_saves manual_pt
     end
 
     def test_backorders
@@ -55,4 +69,21 @@ class SoLineTest < Skr::TestCase
         assert_equal 1, line = skr_sales_orders(:picking).lines.unallocated.count
         assert_equal 0, skr_sales_orders(:complete).lines.unallocated.count
     end
+
+    def test_picking_qty
+        sol = skr_so_lines(:first_string)
+        sol.sku_loc.allocate_available_qty!
+        pt = sol.sales_order.pick_tickets.build
+        assert_saves pt
+        assert_equal 2, sol.reload.qty_picking
+    end
+
+    def test_invoice_qty
+        so = skr_sales_orders(:first)
+        inv = Invoice.new( sales_order: so )
+        inv.lines.from_sales_order( so )
+        assert_saves inv
+        assert_equal inv.lines.first.qty, so.lines.first.qty_invoiced
+    end
+
 end
