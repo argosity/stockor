89068d38b74b7bdd511f7723a4077fd9c9089727
Nathan Stitt <nathan@stitt.org>
2014-04-22 20:27:51 -0500

    additional tests for testing invoicing

--- test/pt_line_test.rb
+++ test/pt_line_test.rb
@@ -2,23 +2,31 @@ require_relative "test_helper"
 
 class PtLineTest < Skr::TestCase
 
+    def test_invoicing
+        pt = skr_pick_tickets(:first)
+        ptl = pt.lines.first
 
-    # def test_invoicing
-    #     pt = pick_tickets(:first)
+        assert_equal 0, ptl.qty_invoiced
 
-    #     inv = Invoice.new({ :pick_ticket=> pt })
-    #     pt.lines.each{ |ptl|
-    #         ptl.qty_to_ship = ptl.qty
-    #         inv.lines.build({ pt_line: ptl })
-    #     }
+        inv = Invoice.new({ :pick_ticket=> pt })
+        inv.lines.from_pick_ticket( pt )
+        assert_saves inv
 
-    #     inv.save!.must_equal true
-    #     inv.lines.length.must_equal pt.lines.length
+        assert_equal inv.lines.length, pt.lines.length
 
-    #     il = inv.lines.first
+        refute_nil inv.lines.first.pt_line
+        assert_equal ptl, inv.lines.first.pt_line
 
-    #     il.pt_line.wont_be_nil
-    #     il.pt_line.qty_shipped.must_equal il.qty
-    # end
+        assert_equal ptl.reload.qty_invoiced, inv.lines.first.qty
+    end
+
+    def test_bin_is_assigned
+        so = skr_sales_orders(:first)
+        skr_sku_locs(:stringdefault).allocate_available_qty!
+        assert_equal 1, so.lines.pickable.count
+        pt = so.pick_tickets.build
+        assert_saves pt
+        assert_equal skr_sku_locs(:stringdefault).bin, pt.lines.first.bin
+    end
 
 end
