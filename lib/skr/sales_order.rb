817ec8711ce103679ee8a3e03b61ba05e34e033f
Nathan Stitt <nathan@stitt.org>
2014-05-30 10:25:10 -0500

    style cleanups

--- lib/skr/sales_order.rb
+++ lib/skr/sales_order.rb
@@ -24,15 +24,15 @@ module Skr
 
         belongs_to :customer, export: true
         belongs_to :location, export: true
-        belongs_to :terms, :class_name=>'PaymentTerm', export: { writable: true }
-        belongs_to :billing_address,  :class_name=>'Address', export: { writable: true }
-        belongs_to :shipping_address, :class_name=>'Address', export: { writable: true }
+        belongs_to :terms, class_name: 'PaymentTerm', export: { writable: true }
+        belongs_to :billing_address,  class_name: 'Address', export: { writable: true }
+        belongs_to :shipping_address, class_name: 'Address', export: { writable: true }
 
         has_many   :lines, ->{ order(:position) }, :class_name=>'SoLine', :inverse_of=>:sales_order,
                    extend: Concerns::SO::Lines, export: { writable: true }
-        has_many   :skus, :through=>:lines
-        has_many   :pick_tickets, :inverse_of=>:sales_order, :before_add=>:setup_new_pt
-        has_many   :invoices,     :inverse_of=>:sales_order, listen: { save: 'on_invoice' }
+        has_many   :skus, through: :lines
+        has_many   :pick_tickets, inverse_of: :sales_order, before_add: :setup_new_pt
+        has_many   :invoices,     inverse_of: :sales_order, listen: { save: 'on_invoice' }
 
         validates :location, :terms, :customer, set: true
         validates :billing_address, :shipping_address, :order_date, presence: true
@@ -51,18 +51,18 @@ module Skr
         # customer_code, customer_name, bill_addr_name, total, num_lines, total_other_charge_amount,
         # total_tax_amount, total_shipping_amount,subtotal_amount
         scope :with_amount_details, lambda { | *args |
-            compose_query_using_detail_view( view: 'so_amount_details', join_to: 'sales_order_id' )
+            compose_query_using_detail_view(view: 'so_amount_details', join_to: 'sales_order_id')
         }, export: true
 
         # joins the so_allocation_details which includes the additional fields:
         # number_of_lines,  allocated_total, number_of_lines_allocated, number_of_lines_fully_allocated
         scope :with_allocation_details, lambda {
-            compose_query_using_detail_view( view: 'so_allocation_details', join_to: 'sales_order_id' )
+            compose_query_using_detail_view(view: 'so_allocation_details', join_to: 'sales_order_id')
         }
 
         # a open SalesOrder is one who's state is not "complete" or "canceled"
         scope :open, lambda { | *args |
-            where( arel_table[:state].not_in ['complete','canceled'] )
+            where( arel_table[:state].not_in ['complete', 'canceled'] )
         }, export: true
 
         # a SalesOrder is allocated if it has one or more lines with qty_allocated>0
@@ -79,14 +79,14 @@ module Skr
         #   ship_partial=true and at least one line is allocated
         #   all lines are fully allocated
         scope :pickable, ->(unused=nil){
-            allocated.where("( ship_partial='t' and details.number_of_lines_allocated > 0 ) " +
-              " or ( details.number_of_lines=details.number_of_lines_fully_allocated)" )
+            allocated.where("( ship_partial='t' and details.number_of_lines_allocated > 0 ) " \
+              " or ( details.number_of_lines=details.number_of_lines_fully_allocated)")
         }, export: true
 
         # @return [Array of Array[day_ago,date, order_count,line_count,total]]
         def self.sales_history( ndays )
-            qry="select * from #{Skr::Core.config.table_prefix}so_dailly_sales_history where days_ago<#{ndays.to_i}"
-            self.connection.execute(qry).values
+            qry = "select * from #{Skr::Core.config.table_prefix}so_dailly_sales_history where days_ago<#{ndays.to_i}"
+            connection.execute(qry).values
         end
 
         state_machine do
@@ -108,7 +108,7 @@ module Skr
             self.order_date = Date.today
         end
 
-        private
+      private
 
         # When the location changes, lines need to have their sku_loc modified to point to the new location as well
         def check_if_location_changed
@@ -138,7 +138,6 @@ module Skr
             true
         end
 
-
         # when the order is canceled, inform the lines
         def cancel_all_lines
             self.pick_tickets.each{ |pt| pt.cancel! }
@@ -156,8 +155,5 @@ module Skr
                 self.shipping_address = customer.shipping_address if self.shipping_address.blank?
             end
         end
-
     end
-
-
 end # Skr module
