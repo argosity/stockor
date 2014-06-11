fbd4c0042ad1ba5e5beafe99cddcf88adfd5a4b8
Nathan Stitt <nathan@stitt.org>
2014-06-10 22:44:42 -0500

    include all lib files by default

--- app/assets/javascripts/skr/lib/index.js
+++ app/assets/javascripts/skr/lib/index.js
@@ -1,5 +1,12 @@
 //= require ./namespace
+//= require_self
 //= require ./noConflict
 //= require ./utilFunctions
+//= require ./underscore.inflection
 //= require ./loader
 //= require ./backbone-sync
+//= require ./ModuleSupport
+//= require_tree .
+
+
+Skr.namespace('lib');
