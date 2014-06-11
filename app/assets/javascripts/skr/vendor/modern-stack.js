21c3a4e901de667544266a906ad8838962ae4816
Nathan Stitt <nathan@stitt.org>
2014-06-10 22:50:48 -0500

    A few more deps

--- app/assets/javascripts/skr/vendor/modern-stack.js
+++ app/assets/javascripts/skr/vendor/modern-stack.js
@@ -3,6 +3,13 @@
 //= require ./backbone
 //= require ./Backbone.ModelBinder
 //= require ./Backbone.CollectionBinder
-//= require ./dataTables
 //= require ./lazyload
 //= require ./jquery.tap
+//= require ./bootstrap/transition
+//= require ./bootstrap/tooltip
+//= require ./bootstrap/popover
+//= require ./bootstrap/modal
+//= require ./bootstrap/dropdown
+//= require ./dataTables
+//= require ./fake-amd
+//= require_self
