e353ff1d127026c7c7f8da98f7b4baabdf1ae717
Nathan Stitt <nathan@stitt.org>
2014-07-20 22:37:39 -0500

    Only include association if it's got data

--- lib/skr/concerns/sanitize_json.rb
+++ lib/skr/concerns/sanitize_json.rb
@@ -12,9 +12,11 @@ module Skr
                 # It returns only the attributes that have been marked as exportable
                 # @param json [Hash]
                 # @param user [UserProxy,User] who is performing request
-                def sanitize_json( json, user )
+                def sanitize_json(json, user = Skr::UserProxy.current)
+                    return {} unless user.can_write?(self)
                     json.each_with_object(Hash.new) do | kv, result |
                         ( key, value ) = kv
+
                         if json_attribute_is_allowed?( key.to_sym, user )
                             result[ key ] = value
                         else
@@ -27,12 +29,13 @@ module Skr
                             klass = klass_name.safe_constantize || "Skr::#{klass_name}".constantize
 
                             # only Hash, Array & nil is valid for nesting attributes
-                            result[ (name + '_attributes').to_sym ] = case value
-                                            when Hash  then klass.sanitize_json( value, user )
-                                            when Array then value.map{ | nested | klass.sanitize_json( nested, user ) }
-                                            else
-                                                nil
-                                            end
+                            cleaned = case value
+                                      when Hash  then klass.sanitize_json( value, user )
+                                      when Array then value.map{ | nested | klass.sanitize_json( nested, user ) }
+                                      else
+                                          nil
+                                      end
+                            result[ (name + '_attributes').to_sym ] = cleaned unless cleaned.blank?
                         end
                     end
                 end
--- test/concerns/sanitize_json_test.rb
+++ test/concerns/sanitize_json_test.rb
@@ -13,34 +13,34 @@ class SanitizeJsonTest < Skr::TestCase
 
     def test_unknown_attribute_removal
         data = { name: 'CASH', number: '1200', drop_table: 'users' }
-        json = Skr::GlAccount.sanitize_json( data, nil )
+        json = Skr::GlAccount.sanitize_json(data)
         assert_equal data.except(:drop_table), json
     end
 
     def test_cleaning_unwanted_attributes
         data = { name: 'CASH', number: '1200',credits: [] }
-        json = Skr::GlAccount.sanitize_json( data, nil )
+        json = Skr::GlAccount.sanitize_json(data)
         data.delete(:credits)
         assert_equal data, json
     end
 
     def test_exported_associations
         data = { foo: 'bar', description: 'a test', debits: [], credits: [] }
-        json = Skr::GlTransaction.sanitize_json( data, nil )
+        json = Skr::GlTransaction.sanitize_json(data)
         assert_equal( { description: 'a test', debits_attributes: [], credits_attributes: [] }, json )
     end
 
     def test_blacklisted_attributes
         GlAccount.send :blacklist_json_attributes, :name
         data = { name: 'CASH', number: '1200' }
-        json = Skr::GlAccount.sanitize_json( data, nil )
+        json = Skr::GlAccount.sanitize_json(data)
         data.delete( :name )
         assert_equal data, json
     end
 
     def test_recursive_cleaning
         data = { gl_transaction: { source: 'unk', credits: [ { account_number: '120001' } ] } }
-        json = GlManualEntry.sanitize_json( data, nil )
+        json = GlManualEntry.sanitize_json(data)
         assert_equal( { gl_transaction_attributes: { credits_attributes: [ { account_number: '120001' } ] } }, json )
     end
 
