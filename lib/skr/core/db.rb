bdb92cd799f29cfc0235b8bfc6f2360f12138a2a
Nathan Stitt <nathan@stitt.org>
2014-07-20 22:38:17 -0500

    DB Configure for rake
    This way it can be called from other gems

--- lib/skr/core/db.rb
+++ lib/skr/core/db.rb
@@ -22,6 +22,20 @@ module Skr
                 Dir.glob("#{migrations_dir}/[0-9]*_*.rb").grep(/\d+_#{file_name}.rb$/).first
             end
 
+            def configure_rake_environment
+                ActiveRecord::Tasks::DatabaseTasks.seed_loader = Skr::Core::DB
+                env = ENV['RAILS_ENV'] || 'development'
+                Skr::Core::DB.config_file ||= 'config/database.yml'
+                ENV['SCHEMA'] ||= 'db/schema.sql'
+                ENV['DB_STRUCTURE'] ||= 'db/schema.sql'
+                ActiveRecord::Base.schema_format = :sql
+                Skr::Core::DB.establish_connection( env )
+                ActiveRecord::Tasks::DatabaseTasks.database_configuration = ActiveRecord::Base.configurations
+                ActiveRecord::Tasks::DatabaseTasks.env = 'test'
+                ActiveRecord::Tasks::DatabaseTasks.migrations_paths = 'db/migrate'
+                ActiveRecord::Tasks::DatabaseTasks.current_config( :config => ActiveRecord::Base.configurations[ env ] )
+            end
+
             def create_migration( migration_name )
                 STDERR.puts "Migration #{migration_name} already exists!" and return false if migration_exists?( migration_name )
                 migration_number = Time.now.utc.strftime("%Y%m%d%H%M%S")
--- tasks/migrations.rake
+++ tasks/migrations.rake
@@ -11,18 +11,7 @@ require 'rake'
 namespace :db do
 
     task :environment do
-        ActiveRecord::Tasks::DatabaseTasks.seed_loader = Skr::Core::DB
-        env = ENV['RAILS_ENV'] || 'development'
-
-        Skr::Core::DB.config_file ||= 'config/database.yml'
-        ENV['SCHEMA'] ||= 'db/schema.sql'
-        ENV['DB_STRUCTURE'] ||= 'db/schema.sql'
-        ActiveRecord::Base.schema_format = :sql
-        Skr::Core::DB.establish_connection( env )
-        ActiveRecord::Tasks::DatabaseTasks.database_configuration = ActiveRecord::Base.configurations
-        ActiveRecord::Tasks::DatabaseTasks.env = 'test'
-        ActiveRecord::Tasks::DatabaseTasks.migrations_paths = 'db/migrate'
-        ActiveRecord::Tasks::DatabaseTasks.current_config( :config => ActiveRecord::Base.configurations[ env ] )
+        Skr::Core::DB.configure_rake_environment
     end
 
     desc "create an ActiveRecord migration"
