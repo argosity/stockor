28271ab814aad980cb10cd6bed8be7ea399df060
Nathan Stitt <nathan@stitt.org>
2015-02-14 19:38:55 -0600

    Don't require demo access from stockor
    When the demo is needed, it can require stockor and act
    as the controlling extension

--- Gemfile
+++ Gemfile
@@ -2,9 +2,6 @@ source 'https://rubygems.org'
 
 gem "lanes", github: "argosity/lanes", branch: 'master'
 
-# Uncomment to enable access control and user administration
-gem "stockor-demo-access",  github: "argosity/stockor-demo-access", branch: 'master'
-
 gem "faker"
 
 gem 'puma'
--- lib/skr/extension.rb
+++ lib/skr/extension.rb
@@ -9,14 +9,6 @@ module Skr
         root_path Pathname.new(__FILE__).dirname.join("..","..").expand_path
         components "record-finder", "select-field"
 
-        def on_boot
-            Lanes::API::Root.before do
-                Thread.current[:demo_user_info] = {
-                  name: session[:name],
-                  email: session[:email]
-                }
-            end
-        end
 
     end
 
