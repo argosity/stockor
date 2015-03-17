ce46b56f6c0955289ce982736bc00c829d548835
Nathan Stitt <nathan@stitt.org>
2015-03-16 21:41:01 -0500

    config for editor

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((js-indent-level . 4)
+         (js2-basic-offset . 4)
+         (enh-ruby-indent-level . 4)
+         (ruby-indent-level . 4)
+         (fill-column . 120))))
+
+((js2-mode . ((js2-basic-offset . 4)
+              (indent-tabs-mode . nil))))
