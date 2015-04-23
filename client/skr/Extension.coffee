2a9e36892404f5ca3307b83ec8e36fe5be25974b
Nathan Stitt <nathan@stitt.org>
2015-04-23 18:10:24 -0500

    Set GL records from bootstrap data

--- client/skr/Extension.coffee
+++ client/skr/Extension.coffee
@@ -1,5 +1,15 @@
-#= require ./screens/customer-maint
-
 class Skr.Extension extends Lanes.Extensions.Base
 
     identifier: "skr"
+
+    # This method is called when the extension is registered
+    # Not all of Lanes will be available yet
+    onRegistered: ->
+
+    # Data that is provided by lib/skr/extension.rb's
+    # client_bootstrap_data method ends up here
+    setBootstrapData: (data)->
+        Skr.Models.GlAccount.initialize(data)
+
+    # All of lanes is loaded and it is in the process of booting
+    onAvailable: ->
--- lib/skr/extension.rb
+++ lib/skr/extension.rb
@@ -9,6 +9,16 @@ module Skr
         root_path Pathname.new(__FILE__).dirname.join("..","..").expand_path
         components "record-finder", "select-field"
 
+        def client_bootstrap_data(view)
+            gl_accounts = Skr::GlAccount.all.as_json
+            {
+                default_gl_account_ids: Hash[ Skr.config.default_gl_accounts.map{ |code,number|
+                    account = gl_accounts.detect{|gla|gla['number'] == number}
+                    [code, account ? account['id'] : 0]
+                }],
+                gl_accounts: gl_accounts
+            }
+        end
 
     end
 
