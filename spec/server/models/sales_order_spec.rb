61f700cf8d417b4f602399dbff9ba7e69542d3a9
Nathan Stitt <nathan@stitt.org>
2015-07-07 21:30:34 -0500

    Better fixture data

--- spec/server/models/sales_order_spec.rb
+++ spec/server/models/sales_order_spec.rb
@@ -6,7 +6,7 @@ class SalesOrderSpec < Skr::TestCase
     it "can be saved" do
         user = Lanes::User.find_by(login:'admin')
         model = SalesOrder.from_attribute_data({
-                    terms_id: skr_payment_term(:cashy).id,
+                    terms_id: skr_payment_term(:cash).id,
                     location_id: skr_location(:default).id,
                     customer_id: skr_customer(:billy).id,
                     billing_address: {
