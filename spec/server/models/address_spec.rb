3d605aaf27341adec071e6cb49a1a7d33120f0b0
Nathan Stitt <nathan@stitt.org>
2015-09-24 19:09:12 -0500

    Test to_s method on address

--- spec/server/models/address_spec.rb
+++ spec/server/models/address_spec.rb
@@ -2,9 +2,10 @@ require_relative '../spec_helper'
 
 class AddressSpec < Skr::TestCase
 
-    it "can be instantiated" do
-        model = Address.new
-        model.must_be_instance_of(Address)
+
+    it "can be converted to plain string" do
+        address = skr_address(:hg_billing).to_s(include: [:email,:phone])
+        assert_equal(address, "Hansel and Gretel\n499 Julianne Radial\nSouth Timmyville WI, 38200\nschuyler@krajcik.name 379-772-4947 x945")
     end
 
 end
