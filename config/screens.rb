f8b3377bdcfe26a0b8aeacdbe772342dcac5d39b
Nathan Stitt <nathan@stitt.org>
2015-11-21 16:43:13 -0600

    Arrange screen model types and groups

--- config/screens.rb
+++ config/screens.rb
@@ -16,11 +16,13 @@ Lanes::Screen.define_group 'customer' do | group |
     group.icon        = "heart"
 end
 Lanes::Screen.for_extension 'Skr' do | screens |
+
+    ## ACCOUNTING GROUP
     screens.define "time-invoicing" do | screen |
         screen.title       = "Time Invoicing"
         screen.description = ""
         screen.icon        = "hourglass"
-        screen.group_id    = "customer"
+        screen.group_id    = "accounting"
         screen.model_class = "Invoice"
         screen.view_class  = "TimeInvoicing"
         screen.js          = "time-invoicing.js"
@@ -36,16 +38,15 @@ Lanes::Screen.for_extension 'Skr' do | screens |
         screen.js          = "customer-projects.js"
         screen.css         = "customer-projects.css"
     end
-    screens.define "time-tracking" do | screen |
-        screen.title       = "Time Tracking"
-        screen.description = ""
-        screen.icon        = "clock-o"
-        screen.group_id    = "hourglass-half"
-        screen.model_class = "SalesOrder"
-        screen.view_class  = "SalesOrder"
-        screen.view_class  = "TimeTracking"
-        screen.js          = "time-tracking.js"
-        screen.css         = "time-tracking.css"
+    screens.define "invoice" do | screen |
+        screen.title       = "Invoice"
+        screen.description = "Invoices"
+        screen.icon        = "money"
+        screen.group_id    = "accounting"
+        screen.model_class = "Invoice"
+        screen.view_class  = "Invoice"
+        screen.js          = "invoice.js"
+        screen.css         = "invoice.css"
     end
     screens.define "customer-maint" do | screen |
         screen.title       = "Customer Maintenance"
@@ -56,6 +57,8 @@ Lanes::Screen.for_extension 'Skr' do | screens |
         screen.js          = 'customer-maint.js'
         screen.css         = 'customer-maint.css'
     end
+
+    # MAINT GROUP
     screens.define "sku-maint" do | screen |
         screen.title       = "SKU Maintenance"
         screen.icon        = "archive"
@@ -74,6 +77,8 @@ Lanes::Screen.for_extension 'Skr' do | screens |
         screen.js          = "vendor-maint.js"
         screen.css         = "vendor-maint.css"
     end
+
+    # CUSTOMER
     screens.define "sales-order" do | screen |
         screen.title       = "Sales Order"
         screen.description = ""
@@ -84,14 +89,14 @@ Lanes::Screen.for_extension 'Skr' do | screens |
         screen.js          = "sales-order.js"
         screen.css         = "sales-order.css"
     end
-    screens.define "invoice" do | screen |
-        screen.title       = "Invoice"
-        screen.description = "Invoices"
-        screen.icon        = "money"
+    screens.define "time-tracking" do | screen |
+        screen.title       = "Time Tracking"
+        screen.description = ""
+        screen.icon        = "hourglass-start"
         screen.group_id    = "customer"
-        screen.model_class = "Invoice"
-        screen.view_class  = "Invoice"
-        screen.js          = "invoice.js"
-        screen.css         = "invoice.css"
+        screen.model_class = "TimeEntry"
+        screen.view_class  = "TimeTracking"
+        screen.js          = "time-tracking.js"
+        screen.css         = "time-tracking.css"
     end
 end
