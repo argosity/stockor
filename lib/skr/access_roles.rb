6fe7184b74c933f8b8a3a5074bfa42f685e4e714
Nathan Stitt <nathan@stitt.org>
2015-11-21 16:43:33 -0600

    New Worker group

--- lib/skr/access_roles.rb
+++ lib/skr/access_roles.rb
@@ -4,22 +4,22 @@ require_relative "model"
 module Lanes::Access
     module Roles
 
-
         # re-open the exising Support role
         class Support
             grant Skr::Customer,
                   Skr::Sku,
-                  Skr::SalesOrder
-
+                  Skr::SalesOrder,
+                  Skr::Invoice,
+                  Skr::TimeEntry
         end
-
 
         class Accounting < Lanes::Access::Role
             grant Skr::Customer,
                   Skr::PaymentTerm,
                   Skr::CustomerProject,
                   Skr::Sku,
-                  Skr::SalesOrder
+                  Skr::SalesOrder,
+                  Skr::TimeEntry
             lock_writes Skr::Customer, :terms
             lock Skr::Sku, :gl_asset_account
             lock Skr::Customer, :gl_receivables_account
@@ -32,6 +32,13 @@ module Lanes::Access
                   Skr::SalesOrder
         end
 
+        class Workforce < Lanes::Access::Role
+            read Skr::Customer,
+                 Skr::Sku
+            grant Skr::SalesOrder,
+                  Skr::Invoice,
+                  Skr::TimeEntry
+        end
     end
 
     Role.grant_global_access(Skr::Address)
