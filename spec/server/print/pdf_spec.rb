102239510fa937bce170dad6db1aa8b831dd2349
Nathan Stitt <nathan@stitt.org>
2015-11-29 22:47:57 -0600

    Updated spec

--- spec/server/print/pdf_spec.rb
+++ spec/server/print/pdf_spec.rb
@@ -1,18 +1,15 @@
 require_relative '../spec_helper'
 
-
 class PrintSpec < Skr::TestCase
 
-
     it "can print" do
-        so = skr_sales_order(:tiny)
-
-        pdf = Skr::Print::PDF.new('sales_order', so.hash_code)
-        assert pdf # generating is commented because that specs fairly slow
-        # assert pdf.output.length > 1000
-        # useful for debugging
-        # File.open('/tmp/test.pdf', 'w'){|f| f.write output.read }
-
+        inv = skr_invoice(:tiny)
+        pdf = Skr::Print::PDF.new('invoice', inv.hash_code)
+        assert(pdf.output.length > 1000)
+        # so = skr_sales_order(:tiny)
+        # generation is turned off for speed of specs
+        # pdf = Skr::Print::PDF.new('sales_order', so.hash_code)
+        # File.open('/tmp/skr-test.pdf', 'w'){|f| f.write pdf.output.read }
     end
 
 end
