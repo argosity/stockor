a47b73072a47ab5469adbc083d0ce5fd2b92d58b
Nathan Stitt <nathan@stitt.org>
2015-12-12 21:42:08 -0600

    Check if period that includes date is locked

--- lib/skr/models/gl_period.rb
+++ lib/skr/models/gl_period.rb
@@ -8,6 +8,12 @@ module Skr
             attr = { year: Time.now.year, period: Time.now.month }
             GlPeriod.where( attr ).first || GlPeriod.create( attr )
         end
+
+        def self.is_date_locked?( date )
+            date && GlPeriod.select(:is_locked).where(
+                year: date.year, period: date.month, is_locked: true
+            ).first
+        end
     end
 
 end
