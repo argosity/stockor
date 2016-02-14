c80cd10ba1e0b83118ba551624fbe85c26e27e4c
Nathan Stitt <nathan@stitt.org>
2016-02-13 19:46:10 -0600

    Join with newline

--- spec/server/models/address_spec.rb
+++ spec/server/models/address_spec.rb
@@ -5,7 +5,7 @@ class AddressSpec < Skr::TestCase
 
     it "can be converted to plain string" do
         address = skr_address(:hg_billing).to_s(include: [:email,:phone])
-        assert_equal(address, "Hansel and Gretel\n499 Julianne Radial\nSouth Timmyville WI, 38200\nschuyler@krajcik.name 379-772-4947 x945")
+        assert_equal(address, "Hansel and Gretel\n499 Julianne Radial\nSouth Timmyville WI, 38200\nschuyler@krajcik.name\n379-772-4947 x945")
     end
 
 end
