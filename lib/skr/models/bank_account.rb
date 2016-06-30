c00b73077f1c37adb8d171b2e122e014e32aef2d
Nathan Stitt <nathan@stitt.org>
2016-06-29 21:45:30 -0500

    add method for getting default bank account

--- lib/skr/models/bank_account.rb
+++ lib/skr/models/bank_account.rb
@@ -8,6 +8,18 @@ module Skr
 
         belongs_to :address,  class_name: 'Skr::Address',
                    export: { writable: true }, dependent: :destroy
+
+        def self.default
+            account = nil
+            if default_id = Skr.system_settings['bank_acount_id']
+                account = BankAccount.find(default_id)
+            end
+            account ||
+                BankAccount.find_by_code( Skr.config.default_bank_account_code ) ||
+                BankAccount.first
+        end
+
     end
+
 
 end
