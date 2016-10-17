1ffa781f105e31eedd72401a9bcccaadab3d5cc7
Nathan Stitt <nathan@stitt.org>
2016-10-16 22:05:50 -0500

    fix path to remote script

--- client/skr/api.js
+++ client/skr/api.js
@@ -1,14 +1,15 @@
 //=require lanes/remote/api
 //=require ./api/namespace
 //=require skr/vendor
-//=require skr/remote
+//=require skr/lib/Remote
 //=require ./api/Models/Base
 //=require_tree ./models/mixins
 //=require ./api/Components/Base
 //=require ./api/Components/Base
 //=require_tree ./api
 //=require_self
-Skr.Remote.configFromScriptTag()
+
+Skr.lib.Remote.configFromScriptTag()
 
 var previousSkr = window.Skr;
 window.Skr = Lanes.Skr;
