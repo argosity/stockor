6be442eccfae80002e25a95c47fa3ade6cabbea9
Nathan Stitt <nathan@stitt.org>
2016-12-12 10:07:12 -0600

    use bogusgateway when not on production

--- lib/skr/merchant_gateway.rb
+++ lib/skr/merchant_gateway.rb
@@ -12,24 +12,20 @@ module Skr
         end
 
         def self._create_gateway
-            unless Lanes.env.production?
+            settings = Lanes::SystemSettings.for_ext('skr-ccgateway')['credit_card_gateway'] || {}
+            if settings.empty? && ! Lanes.env.production?
                 return ActiveMerchant::Billing::BogusGateway.new
             end
-            settings = Lanes::SystemSettings.for_ext('skr-ccgateway')['credit_card_gateway'] || {}
+
             gateway = nil
             if settings['type']
                 gateway = ActiveMerchant::Billing.const_get(settings['type'].classify)
             end
-            if gw.nil?
+            if gateway.nil?
                 Lanes.logger.warn("Unable to find gateway class for id #{settings['type']}")
                 raise ActiveRecord::RecordNotFound
             end
-            gateway.new(settings.except('type'))
-        end
-
-        def purchase(amount, card)
-            raise InvalidCard.new(credit_card.validate) unless credit_card.validate.empty?
-            gw.purchase(amount, card)
+            gateway.new(settings.except('type').symbolize_keys)
         end
 
     end
