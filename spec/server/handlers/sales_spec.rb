2865b4acd32a499324df146e9bb814487b94ae52
Nathan Stitt <nathan@stitt.org>
2017-01-07 18:31:04 -0600

    re-enable specs

--- spec/server/handlers/sales_spec.rb
+++ spec/server/handlers/sales_spec.rb
@@ -7,12 +7,14 @@ class SalesSpec < Skr::ApiTestCase
     let (:authentication) { Lanes::API::AuthenticationProvider.new({}) }
 
     def around
-        config = Lanes::SystemSettings.for_ext('lanes')
+        config = Lanes::SystemSettings.for_ext(:lanes)
         prev = config.dig('smtp', 'from')
         config.smtp ||= {}
         config.smtp['from'] = 'test@test.com'
+        config.persist!
         yield
         config.smtp['from'] = prev
+        config.persist!
     end
 
     let (:data)    {
@@ -29,43 +31,44 @@ class SalesSpec < Skr::ApiTestCase
         }
     }
 
-    # it 'saves and charges when given valid inputs' do
-    #     with_stubbed_payment_proccessor(authorization: 'yep-it-works') do
-    #         assert_difference ->{ Invoice.count }, 1 do
-    #             assert_difference ->{ InvLine.count }, 1 do
-    #                 post '/api/skr/public/sales.json', data
-    #             end
-    #         end
-    #         assert_ok
-    #         invoice = Invoice.find_by_hash_code(json_data.hash_code)
-    #         assert invoice
-    #         assert_equal 'yep-it-works', invoice.payments.first.metadata['authorization']
-    #     end
-    # end
+    it 'saves and charges when given valid inputs' do
+        with_stubbed_payment_proccessor(authorization: 'yep-it-works') do
+            assert_difference ->{ Invoice.count }, 1 do
+                assert_difference ->{ InvLine.count }, 1 do
+                    post '/api/skr/public/sales.json', data
+                end
+            end
+            assert_ok
+            invoice = Invoice.find_by_hash_code(json_data.hash_code)
+            assert invoice
+            assert_equal 'yep-it-works', invoice.payments.first.metadata['authorization']
+        end
+    end
 
-    # it 'sends email' do
-    #     with_stubbed_payment_proccessor(authorization: 'yep-it-works') do
-    #         post '/api/skr/public/sales.json', data
-    #         assert_ok
-    #         invoice = Invoice.find_by_hash_code(json_data.hash_code)
-    #         email = Mail::TestMailer.deliveries.last
-    #         assert_equal email.to, [data[:billing_address][:email]]
-    #         assert_equal email.from, ['test@test.com']
-    #         assert_includes email.subject, 'recent purchase'
-    #         assert_includes email.body, "ID is #{invoice.visible_id}"
-    #     end
-    # end
+    it 'sends email' do
+        with_stubbed_payment_proccessor(authorization: 'yep-it-works') do
+            post '/api/skr/public/sales.json', data
+            assert_ok
+            invoice = Invoice.find_by_hash_code(json_data.hash_code)
+            email = Mail::TestMailer.deliveries.last
+            assert email, 'email failed to send'
 
-    # it 'saves arbitrary json on pdf' do
-    #     custom = data
-    #     custom[:options][:xtrablah] = { one: 1 }
-    #     with_stubbed_payment_proccessor(authorization: 'yep-it-works') do
-    #         post '/api/skr/public/sales.json', custom
-    #     end
-    #     assert_ok
-    #     invoice = Invoice.find_by_hash_code(json_data.hash_code)
-    #     assert_equal( { 'one' => 1 }, invoice.options['xtrablah'] )
-    # end
+            assert_equal email.from, ['test@test.com']
+            assert_includes email.subject, 'recent purchase'
+            assert_includes email.body, "ID is #{invoice.visible_id}"
+        end
+    end
+
+    it 'saves arbitrary json on pdf' do
+        custom = data
+        custom[:options][:xtrablah] = { one: 1 }
+        with_stubbed_payment_proccessor(authorization: 'yep-it-works') do
+            post '/api/skr/public/sales.json', custom
+        end
+        assert_ok
+        invoice = Invoice.find_by_hash_code(json_data.hash_code)
+        assert_equal( { 'one' => 1 }, invoice.options['xtrablah'] )
+    end
 
     it 'can use fields from an event' do
         event = skr_event(:top)
@@ -77,6 +80,7 @@ class SalesSpec < Skr::ApiTestCase
         invoice = Invoice.find_by_hash_code(json_data.hash_code)
         assert invoice.event, 'event is not set'
         email = Mail::TestMailer.deliveries.last
+        assert email, 'email failed to send'
         assert_equal email.from, [event.email_from]
         assert_includes email.body, event.email_signature
         assert 'ticket', invoice.form
